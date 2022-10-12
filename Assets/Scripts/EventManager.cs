008ba7727fd0a8d4cd7ecfbf769db41df300b039
brian babillon <brian.babillon@gmail.com>
2022-10-12 16:26:44 +0200

    add event manager

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class TimedEvent : IGameEvent
+    {
+        public TimedEvent(double time, Component gameEvent)
+        {
+            this.time = time;
+            this.gameEvent = gameEvent;
+        }
+        public double time;
+        public Component gameEvent;
+        public void action()
+        {
+            (gameEvent as IGameEvent)?.action();
+        }
+        public double getTime()
+        {
+            return time;
+        }
+    };
+
+    private int currentEventIndex = 0;
+    private double timer = 0;
+    public List<TimedEvent> timelines = new List<TimedEvent>();
+/*
+    public void addEvent(double time, IGameEvent gameEvent)
+    {
+        timelines.Add(new TimedEvent(time, gameEvent));
+    }
+*/
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private bool isCurrentEventOccuring()
+    {
+        return timelines[currentEventIndex].getTime() < timer;
+    }
+
+
+    private bool hasEventLeft()
+    {
+        return timelines.Count > currentEventIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        while (hasEventLeft() && isCurrentEventOccuring())
+        {
+            var currentEvent = timelines[currentEventIndex++];
+            currentEvent.action();
+        }
+
+    }
+}
